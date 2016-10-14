f88494cfdb48b2a1424f828a56c34b9468a5e96e
Julian Bauer <julian@overnice.de>
2016-10-14 17:07:30 +0200

    new variables + fixed particles

--- variables/variables.ts
+++ variables/variables.ts
@@ -1,7 +1,14 @@
-export const green: string = '#27AE60'
-export const darkGray: string = '#1B1F25'
-export const darkerGray: string = '#171A1F'
 
+/* Accent Colors */
+export const green: string = '#27AE60'
+export const blue: string = '#2A7ED2'
+export const darkBlue: string = '#172A3A'
+export const darkerBlue: string = '#0F202E'
+
+/* Grays (mostly for overlays) */
+
+export const gray04: string = 'rgba(0,0,0,0.04)'
+export const gray07: string = 'rgba(0,0,0,0.07)'
 export const gray10: string = 'rgba(0,0,0,0.1)'
 export const gray20: string = 'rgba(0,0,0,0.2)'
 export const gray30: string = 'rgba(0,0,0,0.3)'
@@ -12,6 +19,8 @@ export const gray70: string = 'rgba(0,0,0,0.7)'
 export const gray80: string = 'rgba(0,0,0,0.8)'
 export const gray90: string = 'rgba(0,0,0,0.9)'
 
+// Basic Size Grid
+
 export const size04: string = '4px'
 export const size06: string = '6px'
 export const size10: string = '10px'
@@ -20,3 +29,9 @@ export const size25: string = '25px'
 export const size38: string = '38px'
 export const size60: string = '60px'
 export const size96: string = '96px'
+
+
+// Additional Sizes
+
+export const size12: string = '12px'
+export const size14: string = '14px'
