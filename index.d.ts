662c2d149aa141fb99b6cb2d68cc8efc2136a067
Johannes Schickling <schickling.j@gmail.com>
2016-10-23 16:13:47 +0200

    fix typings

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
-import _Icon from './dist/components/Icon/Icon'
+export { default as Icon } from './dist/components/Icon/Icon'
+
 import * as _particles from './dist/particles.css'
 import * as _virtual from './dist/virtual.css'
 import * as _variables from './dist/variables/variables'
 
-export import Icon = _Icon
 export import particles = _particles
 export import virtual = _virtual
 export import variables = _variables
