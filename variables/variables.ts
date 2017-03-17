dd5c5639c80fb82004b965b88ece02d42801a621
Julian Bauer <julian@overnice.de>
2017-03-17 17:23:55 +0100

    new icons

--- variables/variables.ts
+++ variables/variables.ts
@@ -27,6 +27,7 @@ export const lightGreen30: string = 'rgba(42,189,60,.3)'
 export const lightGreen50: string = 'rgba(42,189,60,.5)'
 
 export const blue0: string = 'rgba(42,126,210,0)'
+export const blue10: string = 'rgba(42,126,210,.1)'
 export const blue20: string = 'rgba(42,126,210,.2)'
 export const blue50: string = 'rgba(42,126,210,.5)'
 export const blue80: string = 'rgba(42,126,210,.8)'
